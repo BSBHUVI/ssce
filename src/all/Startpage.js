7213e5428bc63f5a52444c5cc5e23ebf1301d1f4
bhuvan <bhuvansbhuvan467@gmail.com>
2022-05-02 14:33:36 +0530

    f

--- src/all/Startpage.js
+++ src/all/Startpage.js
@@ -5,8 +5,11 @@ function Startpage() {
   return (
     <div className='start'>
     <h1>SRI SAIRAM COLLEGE OF ENGINEERING</h1>
-    <img width='1500' height='500' src="https://cache.careers360.mobi/media/presets/720X480/colleges/social-media/media-gallery/3177/2018/10/30/Campus%20View%20of%20Sri%20Sairam%20College%20of%20Engineering%20Bangalore_Campus-View.jpg" alt="sairam" />
+    <div className="img">
 
+  
+    <img src="https://cache.careers360.mobi/media/presets/720X480/colleges/social-media/media-gallery/3177/2018/10/30/Campus%20View%20of%20Sri%20Sairam%20College%20of%20Engineering%20Bangalore_Campus-View.jpg" alt="sairam" />
+    </div>
  
       
     </div>
